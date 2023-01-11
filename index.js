9d0dc995daf7487ce48dc0e96c950d41619bedd0
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-01-11 12:13:29 -0500

    deploy ready fix

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from "express";
+import db from "./connection.js";
 import credits from "./db/credits.json" assert { type: "json" };
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+db.on("connected", () => {
+  app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`);
+  });
 });
 
 app.get("/", (req, res) => {
-  res.json(credits);
-});
-
-app.get("/weee", (req, res) => {
   res.json(credits);
 });
